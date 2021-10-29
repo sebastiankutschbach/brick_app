a9fb8daef6486cc585f2503157f567898f2d6d61
Sebastian Kutschbach <sebastian.kutschbach@freenet.de>
2021-10-29 22:24:10 +0200

    use dotenv for credentials in integration test

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,33 +1,30 @@
-import 'dart:io';
-
 import 'package:brick_app/main.dart' as app;
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
-  final env = Platform.environment;
-  print(env.keys);
 
   group('end-to-end test', () {
     String? username;
     String? password;
     String? apiKey;
 
-    setUp(() {
-      final env = Platform.environment;
-      print(env.keys);
-      username = env['IT_USERNAME'];
-      password = env['IT_PASSWORD'];
-      apiKey = env['IT_APIKEY'];
+    setUp(() async {
+      await dotenv.load();
+      username = dotenv.env['IT_USERNAME'];
+      password = dotenv.env['IT_PASSWORD'];
+      apiKey = dotenv.env['IT_APIKEY'];
     });
 
     testWidgets('login is possible', (WidgetTester tester) async {
       expect(username, isNotNull,
           reason: 'IT_USERNAME not set as env variable');
-      expect(password, isTrue, reason: 'IT_PASSWORD not set as env variable');
-      expect(apiKey, isTrue, reason: 'IT_APIKEY not set as env variable');
+      expect(password, isNotNull,
+          reason: 'IT_PASSWORD not set as env variable');
+      expect(apiKey, isNotNull, reason: 'IT_APIKEY not set as env variable');
 
       app.main();
       await tester.pumpAndSettle();
@@ -46,7 +43,6 @@ void main() {
       await tester.tap(find.byKey(Key('login')));
 
       await tester.pumpAndSettle();
-      await tester.pump(Duration(seconds: 20));
       expect(find.byKey(Key('setList')), findsOneWidget);
     });
   });
