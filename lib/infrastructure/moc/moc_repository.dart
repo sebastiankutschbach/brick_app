89f4334aa7bd61f91f5702fe6d4c2b85206220dd
Sebastian Kutschbach <sebastian.kutschbach@freenet.de>
2022-01-29 10:30:56 +0100

    implemented way to query available moc instructions in advance

--- lib/infrastructure/moc/moc_repository.dart
+++ lib/infrastructure/moc/moc_repository.dart
@@ -1,22 +1,23 @@
 import 'dart:developer';
-import 'dart:io';
+import 'dart:typed_data';
 import 'package:brick_app/core/failure.dart';
 import 'package:dartz/dartz.dart';
 import 'package:http/http.dart';
-import 'package:path_provider/path_provider.dart';
 
 abstract class MocRepositoryFacade {
-  Future<Either<Failure, File>> getBuildInstruction(
+  Future<List<String>> areBuildInstructionsAvailable(
+      {required String setNum, required List<String> mocNums});
+
+  Future<Either<Failure, Uint8List>> getBuildInstruction(
       {required String setNum, required String mocNum});
 }
 
 class MocRepository implements MocRepositoryFacade {
   @override
-  Future<Either<Failure, File>> getBuildInstruction(
+  Future<Either<Failure, Uint8List>> getBuildInstruction(
       {required String setNum, required String mocNum}) async {
-    final directory = await getApplicationDocumentsDirectory();
-
     final urlPath = 'https://brickapp.sebku.de/mocs/$setNum/$mocNum.pdf';
+    print(urlPath);
     final response = await get(Uri.parse(urlPath));
     if (response.statusCode != 200) {
       final String errMsg =
@@ -24,9 +25,22 @@ class MocRepository implements MocRepositoryFacade {
       log(errMsg);
       return left(Failure(errMsg));
     }
-    final downloadedFile = File('${directory.path}/$mocNum.pdf')
-      ..createSync()
-      ..writeAsBytesSync(response.bodyBytes);
-    return right(downloadedFile);
+
+    return right(response.bodyBytes);
+  }
+
+  @override
+  Future<List<String>> areBuildInstructionsAvailable(
+      {required String setNum, required List<String> mocNums}) async {
+    final results = <String>[];
+    for (final mocNum in mocNums) {
+      final urlPath = 'https://brickapp.sebku.de/mocs/$setNum/$mocNum.pdf';
+      final response = await head(Uri.parse(urlPath));
+
+      if (response.statusCode == 200) {
+        results.add(mocNum);
+      }
+    }
+    return results;
   }
 }
--- test/infrastructure/moc/moc_repository_test.dart
+++ test/infrastructure/moc/moc_repository_test.dart
@@ -5,27 +5,57 @@ import 'package:brick_app/infrastructure/moc/moc_repository.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 main() {
-  test('downloads an existing moc build instruction', () async {
-    final MocRepositoryFacade mocRepository = MocRepository();
+  late MocRepositoryFacade mocRepository;
 
-    final response = await mocRepository.getBuildInstruction(
-        setNum: '10696-1', mocNum: 'MOC-93758');
+  setUp(() {
+    mocRepository = MocRepository();
+  });
 
-    response.fold((failure) => fail('Should not throw an error'),
-        (downloadedFile) async {
-      expect(await downloadedFile.exists(), true);
-      downloadedFile.deleteSync();
+  group('get build instructions', () {
+    test('downloads an existing moc build instruction', () async {
+      final response = await mocRepository.getBuildInstruction(
+          setNum: '10696-1', mocNum: 'MOC-93758');
+
+      response.fold((failure) => fail('Should not throw an error'),
+          (downloadedBytes) async {
+        expect(downloadedBytes.length, greaterThan(0));
+      });
+    });
+
+    test(
+        'returns failure when requesting an non-existing moc build instruction',
+        () async {
+      final response = await mocRepository.getBuildInstruction(
+          setNum: 'doesnotexist-1', mocNum: 'doesnotexist-93758');
+
+      response.fold((failure) => expect(failure, isNotNull),
+          (downloadedFile) => fail('Should have thrown an error instead'));
     });
   });
 
-  test('returns failure when requesting an non-existing moc build instruction',
-      () async {
-    final MocRepositoryFacade mocRepository = MocRepository();
+  group('are build instructions available', () {
+    test('only returns moc nums for which are build instructions available',
+        () async {
+      expect(
+          await mocRepository.areBuildInstructionsAvailable(
+              setNum: '10696-1', mocNums: ['MOC-93758', 'MOC-12345']),
+          ['MOC-93758']);
+    });
 
-    final response = await mocRepository.getBuildInstruction(
-        setNum: 'doesnotexist-1', mocNum: 'doesnotexist-93758');
+    test('returns empty list for set without mocs', () async {
+      expect(
+        await mocRepository.areBuildInstructionsAvailable(
+            setNum: '854-1', mocNums: ['MOC-93758', 'MOC-12345']),
+        [],
+      );
+    });
 
-    response.fold((failure) => expect(failure, isNotNull),
-        (downloadedFile) => fail('Should have thrown an error instead'));
+    test('returns empty list for non existing set num', () async {
+      expect(
+        await mocRepository.areBuildInstructionsAvailable(
+            setNum: '11111-1', mocNums: ['MOC-93758', 'MOC-12345']),
+        [],
+      );
+    });
   });
 }
