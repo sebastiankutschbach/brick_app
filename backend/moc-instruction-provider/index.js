d7e1c3e99a81a8e06b147a29225198bff0bdf0ea
Sebastian Kutschbach <sebastian.kutschbach@freenet.de>
2022-01-30 10:32:34 +0100

    handle object not found before creating a presigned url

--- backend/moc-instruction-provider/index.js
+++ backend/moc-instruction-provider/index.js
@@ -4,27 +4,46 @@ exports.handler = async (event) => {
     const eventPath = event['path'];
     console.log(`eventPath: ${eventPath}`);
     const splittedPath = eventPath.split('/');
-    
+
     const setNum = splittedPath[2];
     console.log(`setNum: ${setNum}`);
     const mocNum = splittedPath[4];
     console.log(`mocNum: ${mocNum}`);
-    
+
     const key = `${setNum}/${mocNum}.pdf`;
     console.log(key);
-        
+
     const s3 = new AWS.S3();
     var params = {
-        Bucket : 'brickapp-mocs',
-        Key : key
+        Bucket: 'brickapp-mocs',
+        Key: key
     }
-    const response = await s3.getSignedUrlPromise('getObject', params);
-    
-    console.log('data' + response);
-    return {
-        isBase64Encoded: false,
-        statusCode: 200,
-        headers: {},
-        body: JSON.stringify(response)
+    try {
+        await s3.headObject(params).promise();
+        const response = await s3.getSignedUrlPromise('getObject', params);
+        console.log('data' + response);
+        return {
+            isBase64Encoded: false,
+            statusCode: 200,
+            headers: {},
+            body: JSON.stringify(response)
+        }
+    } catch (error) {
+        if (error.name === 'NotFound') {
+            return {
+                isBase64Encoded: false,
+                statusCode: 404,
+                headers: {},
+                body: JSON.stringify(error.name)
+            }
+        } else {
+            return {
+                isBase64Encoded: false,
+                statusCode: 500,
+                headers: {},
+                body: JSON.stringify(error.name)
+            }
+        }
     }
+
 };
